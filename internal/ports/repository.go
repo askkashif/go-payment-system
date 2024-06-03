e01bdcbab9d8ea75872bb0a6ce52c558e6ad2146
Kashif Raza <71992070+askkashif@users.noreply.github.com>
2024-06-03 19:44:11 +0100

    Update repository.go

--- internal/ports/repository.go
+++ internal/ports/repository.go
@@ -2,9 +2,17 @@ package ports
 
 import "payment-system-one/internal/models"
 
+// Repository defines the interface for interacting with data storage.
 type Repository interface {
+	// FindUserByEmail retrieves a user by their email address.
 	FindUserByEmail(email string) (*models.User, error)
+
+	// TokenInBlacklist checks if a token is in the blacklist.
 	TokenInBlacklist(token *string) bool
+
+	// CreateUser creates a new user in the database.
 	CreateUser(user *models.User) error
+
+	// UpdateUser updates an existing user in the database.
 	UpdateUser(user *models.User) error
 }
